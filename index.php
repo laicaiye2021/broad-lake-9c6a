b267f23689f91748de2cd6fc229619551a5888ce
laicaiye2021 <108520671+laicaiye2021@users.noreply.github.com>
2022-07-01 18:18:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "<html>\n";
+echo "   <head>\n";
+echo "       <title>laicaiye</title>\n";
+echo "       <meta charset=\"utf-8\">\n";
+echo "       <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\">\n";
+echo "       <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0\">\n";
+echo "   </head>\n";
+echo "   <body>\n";
+
+echo "   </body>\n";
+echo "</html>\n";
+?>
